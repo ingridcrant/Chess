7ac9ee3941f49500d8654d54b60d205366ab4098
Ingrid Crant <ingridcrant@gmail.com>
2022-12-02 11:04:56 -0500

    skips Two Move

--- pieces/pawn.cc
+++ pieces/pawn.cc
@@ -11,8 +11,17 @@ bool Pawn::getSkipsTwo() {
 void Pawn::generateNextPositions(Board* board) {
     // TO DO:
     // 1. skips two move - DONE
+    int dRow = (colour == BLACK) ? 1 : -1;
+    if (firstMove) {
+        Position skipTwo = Position{currPos.row, currPos.col + 2 * dx};
+        if (board->getPos(skipTwo.row - dRow, skipTwo.col) == nullptr && board->getPos(skipTwo.row, skipTwo.col) == nullptr) {
+            nextPositions[skipTwo] = SKIP_TWO;
+        }
+    }
+
     // 2. en passant
 
+    // 3. regular motion
     for (Direction d : directions) {
         std::map<Position, MoveTypes> nextPositionsInD = this->allPosInDirection(d, board);
         nextPositions.insert(nextPositionsInD.begin(), nextPositionsInD.end());
