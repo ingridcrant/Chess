aace8350350061bc19f15573997e58e11ecb9846
a332chen <a332chen@uwaterloo.ca>
2022-12-04 14:20:56 -0500

    change dimensions of graphics

--- display/window.h
+++ display/window.h
@@ -12,7 +12,7 @@ class Xwindow {
   unsigned long colours[10];
 
  public:
-  Xwindow(int width=500, int height=500);  // Constructor; displays the window.
+  Xwindow(int width=1000, int height=1000);  // Constructor; displays the window.
   ~Xwindow();                              // Destructor; destroys the window.
   Xwindow(const Xwindow&) = delete;
   Xwindow &operator=(const Xwindow&) = delete;
